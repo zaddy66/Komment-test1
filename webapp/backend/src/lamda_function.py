4c23dc7cc54ffc9e880f2f7dc4cece50cbdae8b7
Suraj <yme9833@gmail.com>
2026-03-25 13:10:57 +0530

    final commit

--- webapp/backend/src/lamda_function.py
+++ webapp/backend/src/lamda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! from github')
+        'body': json.dumps('Hello from Lambda! from github Actions')
     }
